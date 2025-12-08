ee28907917ea8e40ae42a18c2cbc5cb17f98d5e3
thopn3 <thopn3@fpt.edu.vn>
2025-12-08 11:28:23 +0700

    Express router, controller, params

--- Topic3_ExpressFramework/controllers/books.controller.js
+++ Topic3_ExpressFramework/controllers/books.controller.js
@@ -20,6 +20,57 @@ async function getBooks(req, resp){
     }
 }
 
+// Get Book details
+const getBookDetails = async (req, resp) => {
+    try {
+        const {id} = req.params;
+        const singleBook = await books?.find(b => b.id === id);
+        
+        if(!singleBook)
+            resp.status(404).json({message: 'Not found'});
+
+        const formattedBook = {
+            bookId: singleBook.id, 
+            title: singleBook.title, 
+            rYear: singleBook.releaseYear, 
+            authors: singleBook?.authors.map(a => a?.name), 
+            publisher: singleBook.publisher.name
+        };
+        
+        resp.status(200).json(formattedBook);
+    } catch (error) {
+        resp.status(500).json({message: `Error: ${error.message}`});
+    }
+}
+
+const getAuthorDetailByBookId = async (req, resp)=>{
+    try {
+        const {id, auId} = req.params;
+        // Tìm sách theo id
+        const book = await books.find(b => b.id === id);
+        
+        if(!book)
+            resp.status(404).json({message: 'Book not found'});
+        
+        const author = book.authors.find(a => a.id === auId);
+        
+        if(!author)
+            resp.status(404).json({message: 'Author not found'});
+
+        const authorDetails = authors.find(a => a.id === auId);
+        
+        resp.status(200).json({
+            authorId: authorDetails.id,
+            authorName: authorDetails.name,
+            birdYead: authorDetails.birthYear
+        });
+    } catch (error) {
+        resp.status(500).json({message: `Error: ${error.message}`});
+    }
+}
+
 module.exports = {
-    getBooks
+    getBooks,
+    getBookDetails,
+    getAuthorDetailByBookId
 }
--- Topic3_ExpressFramework/routes/books.route.js
+++ Topic3_ExpressFramework/routes/books.route.js
@@ -7,6 +7,10 @@ router.use(express.json());
 
 // GET / - Get all books includes Author and Publisher
 router.get('/', bookController.getBooks);
+// GET /:id - Get a single Book
+router.get('/:id', bookController.getBookDetails);
+// GET /:id/author/:auId - Get details of author theo auId tham gia viết cuốn sách có :id tương ứng
+router.get('/:id/author/:auId', bookController.getAuthorDetailByBookId);
 
 
 module.exports = router;
